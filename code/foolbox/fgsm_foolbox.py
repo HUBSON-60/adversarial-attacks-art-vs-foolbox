18f28059a83a63059fcf70124708ec913b3507e1
HUBSON-60 <hizydorski@gmail.com>
2026-07-18 23:51:45 +0200

    Simplify dataset loading and FGSM attack process
    Refactor dataset loading and streamline FGSM attack execution.

--- code/foolbox/fgsm_foolbox.py
+++ code/foolbox/fgsm_foolbox.py
@@ -25,40 +25,25 @@ model.eval()
 
 transform = transforms.ToTensor()
 
-dataset = datasets.MNIST(
-    root="./data",
-    train=False,
-    download=True,
-    transform=transform
-)
-
+dataset = datasets.MNIST(root="./data", train=False, download=True, transform=transform)
 
 # Pobranie pierwszych 5 obrazków
 images = torch.stack([dataset[i][0] for i in range(5)])
 labels = torch.tensor([dataset[i][1] for i in range(5)])
 
-
 print("Obrazki i etykiety wczytane!")
 print("images.shape:", images.shape)
 print("labels:", labels)
 
+fmodel = fb. PyTorchModel (model, bounds=(0,1))
 
-fmodel = fb.PyTorchModel(model, bounds=(0,1))
+attack fb.attacks.FGSM() 
+adv_images_list = attack(fmodel, images, labels, epsilons=0.3) # zwraca listę
+adv_images = adv_images_list[0]	# wybieramy pierwszy (i jedyny) epsilon
 
-attack = fb.attacks.FGSM()
+print("Atak FGSM wykonany!") 
 
-adv_images_list = attack(
-    fmodel,
-    images,
-    labels,
-    epsilons=0.3
-)
-
-adv_images = adv_images_list[0]
-
-print("Atak FGSM wykonany!")
-
-images_np = images.numpy()
+images_np = images.numpy() 
 adv_images_np = adv_images.numpy()
 
 plt.figure(figsize=(10,2))
@@ -66,16 +51,15 @@ plt.figure(figsize=(10,2))
 for i in range(5):
 
     # Oryginalny
-    plt.subplot(2,5,1+i)
+    plt.subplot(2,5,i+1)
     plt.imshow(images_np[i].squeeze(), cmap="gray")
     plt.axis("off")
 
     if i == 0:
         plt.title("Oryginalny")
 
-
     # Adversarial
-    plt.subplot(2,5,6+i)
+    plt.subplot(2,5,i+6)
     plt.imshow(adv_images_np[i].squeeze(), cmap="gray")
     plt.axis("off")
 
