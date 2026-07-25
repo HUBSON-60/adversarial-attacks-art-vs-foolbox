98aaf847371ee9b344a0f2974e6e7dce949cd918
HUBSON-60 <hizydorski@gmail.com>
2026-07-25 18:50:33 +0200

    Load MNIST dataset and implement FGSM attack
    Refactored code to load MNIST dataset and create adversarial examples using FGSM.

--- code/art/fgsm_art.py
+++ code/art/fgsm_art.py
@@ -1,33 +1,74 @@
 import torch
 import torch.nn as nn
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
 
-# prosty model CNN (jedna warstwa linearna dla MNIST)
+from art.estimators.classification import PyTorchClassifier
+from art.attacks.evasion import FastGradientMethod
+
+
+# Prosty model klasyfikacyjny dla MNIST
 model = nn.Sequential(
-         nn.Flatten(),
-
-         nn.Linear(28*28, 10)
+    nn.Flatten(),
+    nn.Linear(28 * 28, 10)
 )
 
+model.eval()
+
+
+# Funkcja straty i optymalizator wymagane przez ART
 loss_fn = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
 
 
-# dummy dane testowe (10 obrazów 28x28)
-x_test = torch.rand((10, 1, 28, 28))
-y_test = torch.randint(0, 10, (10,))
+# Wczytanie zbioru MNIST
+transform = transforms.ToTensor()
 
-# konwersja na NumPy (ART wymaga)
+dataset = datasets.MNIST(
+    root="./data",
+    train=False,
+    download=True,
+    transform=transform
+)
+
+
+# Pobranie 10 obrazów testowych
+x_test = torch.stack([dataset[i][0] for i in range(10)])
+y_test = torch.tensor([dataset[i][1] for i in range(10)])
+
+
+print("MNIST załadowany!")
+print("x_test shape:", x_test.shape)
+print("y_test:", y_test)
+
+
+# ART wymaga danych w formacie NumPy
 x_test_np = x_test.numpy()
 y_test_np = y_test.numpy()
 
-from art.estimators.classification import PyTorchClassifier
-classifier = PyTorchClassifier(model=model, loss=loss_fn, optimizer=optimizer,
-    				  input_shape=(1, 28, 28), nb_classes=10)
+
+# Utworzenie klasyfikatora ART
+classifier = PyTorchClassifier(
+    model=model,
+    loss=loss_fn,
+    optimizer=optimizer,
+    input_shape=(1, 28, 28),
+    nb_classes=10
+)
 
 
-from art.attacks.evasion import FastGradientMethod
+# Atak FGSM
+attack = FastGradientMethod(
+    estimator=classifier,
+    eps=0.2
+)
 
-attack = FastGradientMethod(estimator=classifier, eps=0.2)
-x_test_adv = attack.generate(x=x_test_np)
 
-print("FGSM wykonany, x_test_adv shape:", x_test_adv.shape)
+# Generowanie przykładów adwersarialnych
+x_test_adv = attack.generate(
+    x=x_test_np
+)
+
+
+print("FGSM wykonany!")
+print("x_test_adv shape:", x_test_adv.shape)
